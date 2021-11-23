03b2dd3601377f90c30d7e5331c8f244b82884da
SomeoneThatForkAnythingHeNeeds <94541840+SomeoneThatForkAnythingHeNeeds@users.noreply.github.com>
2021-11-23 22:33:10 +0200

    Update Tic-Tac-Toe.js

--- src/Tic-Tac-Toe.js
+++ src/Tic-Tac-Toe.js
@@ -87,7 +87,7 @@ module.exports = class TicTacToe {
         }
 
         if (this.opponent.bot) return this.sendMessage(this.options.botsMessage)
-        // if (this.opponent.id === this.message.author.id) return this.sendMessage(this.options.yourselfMessage)
+        if (this.opponent.id === this.message.author.id) return this.sendMessage(this.options.yourselfMessage)
 
         const check = await verify(this.options)
 
