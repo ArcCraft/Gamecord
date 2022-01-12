efa84a786ae9a44e32061bfc349e40aa3d2076f6
Forker <94541840+forker20@users.noreply.github.com>
2022-01-13 00:02:22 +0200

    Update Snake.js

--- src/Snake.js
+++ src/Snake.js
@@ -226,7 +226,7 @@ if(!options.overembed) options.overembed = {};
            if(this.score > data.score) {
            this.game.set({key: `${this.message.author.id}_status`, value: {score: this.score, lvl: data.lvl}});
            if(myIndex < 0) {
-        obj.push({user: `<@${this.message.author.id}>`, {score: this.score, lvl: data.lvl}});
+        obj.push({user: `<@${this.message.author.id}>`, score: this.score, lvl: data.lvl});
         this.game.set('snake_lb', obj)
       } else {   
 obj[myIndex].score = this.score;
@@ -236,7 +236,7 @@ this.game.set('snake_lb', obj)
            } else if(this.lvl > data.lvl) {
            this.game.set({key: `${this.message.author.id}_status`, value: {score: data.score, lvl: this.lvl}});
            if(myIndex < 0) {
-        obj.push({user: `<@${this.message.author.id}>`, {score: data.score, lvl: this.lvl}});
+        obj.push({user: `<@${this.message.author.id}>`, score: data.score, lvl: this.lvl});
         this.game.set('snake_lb', obj)
       } else {   
 obj[myIndex].score = data.score;
@@ -249,7 +249,7 @@ this.game.set('snake_lb', obj)
      let obj = this.game.get('snake_lb');
       let myIndex = obj.findIndex(v => v.user === `<@${this.message.author.id}>`);
       if(myIndex < 0) {
-        obj.push({user: `<@${this.message.author.id}>`, {score: this.score, lvl: this.lvl}});
+        obj.push({user: `<@${this.message.author.id}>`, score: this.score, lvl: this.lvl});
         this.game.set('snake_lb', obj)
       } else {   
 obj[myIndex].score = this.score;
